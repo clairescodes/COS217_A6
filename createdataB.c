56f5a6ce3c20f8213606ace8839467fe84aeed49
Jeonghyun Shin <js9523@princeton.edu>
2024-12-05 21:16:49 -0500

    createdataB format

---
+++ createdataB.c
@@ -0,0 +1,15 @@
+/* file comment needed */
+
+#include <stdio.h>
+#include <string.h>
+
+/* function comment needed */
+
+int main() { 
+    /* write my name to the dataB file */
+
+    /* null byte for name termination */
+
+    /* overrun buffer via padding */
+
+} 
